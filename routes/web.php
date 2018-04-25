8f08bb7fd9e1453852b2f0071d60fad7c67080ba
FunkyToc <vaysse.jeanbaptiste@gmail.com>
2018-04-25 11:05:09 +0200

    redir /admin/{any} -> /admin/

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,18 @@ Route::any('/admin/users/', 'GoyController@users')->name('admin_manage_users');
 Route::any('/admin/texts/', 'GoyController@texts')->name('admin_manage_texts');
 Route::any('/admin/login/', 'GoyController@login')->name('admin_login');
 Route::any('/admin/logout/', 'GoyController@logout')->name('admin_logout');
-Route::any('/admin/active/user/{id}', 'GoyController@activeUser')->where('id', '[0-9]+')->name('admin_active_user');
-Route::any('/admin/active/text/{id}', 'GoyController@activeText')->where('id', '[0-9]+')->name('admin_active_text');
+Route::any('/admin/active/user/{id}', 'GoyController@activeUser')->where(['id' => '[0-9]+'])->name('admin_active_user');
+Route::any('/admin/active/text/{id}', 'GoyController@activeText')->where(['id' => '[0-9]+'])->name('admin_active_text');
+
+// Redirect All Admin Other Routes to the Home Page 
+Route::any('/admin/{any}', function ($any) 
+{
+	return redirect('/admin/');
+})->where(['any' => '.*']);
 
 // Redirect All Other Routes to the Home Page 
 Route::any('/{any}', function ($any) 
 {
-    
 	return redirect('/');
-})->where('any', '.*');
+})->where(['any' => '.*']);
 
